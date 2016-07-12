585cb0cee4abb8da1bd309bcc87dfd4949d9b863
Olivia Taylor <obt1@pitt.edu>
2016-07-12 15:08:47 -0400

    Update zookeeper.sql
    SELECT basic challenges done

--- zookeeper.sql
+++ zookeeper.sql
@@ -1,1 +1,28 @@
 --SELECT basics - challenge 1
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+--SELECT basics - challenge 2
+SELECT name, population FROM world
+  WHERE name IN ('Ireland', 'Iceland', 'Denmark');
+  
+--SELECT basics - challenge 3
+  
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+  
+--SELECT name - challenge 1  
+--SELECT name - challenge 2
+--SELECT name - challenge 3
+--SELECT name - challenge 4
+--SELECT name - challenge 5
+--SELECT name - challenge 6
+--SELECT name - challenge 7
+--SELECT name - challenge 8
+--SELECT name - challenge 9
+--SELECT name - challenge 10
+--SELECT name - challenge 11
+--SELECT name - challenge 12
+--SELECT name - challenge 13
+--SELECT name - challenge 14
+--SELECT name - challenge 15
